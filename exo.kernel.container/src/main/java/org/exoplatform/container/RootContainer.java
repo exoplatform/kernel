52b0cd303aab7f95c2f5985a912bc64063252cba
Dmytro Katayev <dmytro.katayev@exoplatform.com>
2009-11-04 08:36:42 +0000

    EXOJCR-194 log message level changed to log.info (line 431)

--- exo.kernel.container/src/main/java/org/exoplatform/container/RootContainer.java
+++ exo.kernel.container/src/main/java/org/exoplatform/container/RootContainer.java
@@ -428,7 +428,7 @@ public class RootContainer extends ExoContainer
                else
                {
                   booting = true;
-                  log.error("Booting root container with id " + RootContainer.class.hashCode() + "");
+                  log.info("Booting root container with id " + RootContainer.class.hashCode() + "");
                   log.info("Building root container");
                   long time = -System.currentTimeMillis();
                   result = buildRootContainer();
