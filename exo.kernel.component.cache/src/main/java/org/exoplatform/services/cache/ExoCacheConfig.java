bf800fffb89a5d4941547921d4ce46cd763d6757
Dmytro Katayev <dmytro.katayev@exoplatform.com>
2009-11-26 14:10:48 +0000

    EXOJCR-268 Remove type from ExoCacheConfig since it is useless, Javadoc added to ExoCacheConfig

--- exo.kernel.component.cache/src/main/java/org/exoplatform/services/cache/ExoCacheConfig.java
+++ exo.kernel.component.cache/src/main/java/org/exoplatform/services/cache/ExoCacheConfig.java
@@ -19,28 +19,53 @@
 package org.exoplatform.services.cache;
 
 /**
+ * This class defines the main configuration properties of an {@link org.exoplatform.services.cache.ExoCache}
+ * 
  * @author Tuan Nguyen (tuan08@users.sourceforge.net)
  * @since Feb 20, 2005
  * @version $Id: ExoCacheConfig.java 5799 2006-05-28 17:55:42Z geaz $
  */
 public class ExoCacheConfig implements Cloneable
 {
+   /**
+    * The name of the cache.
+    */
    private String name;
 
+   /**
+    * The label of the cache.
+    */
    private String label;
 
+   /**
+    * The maximum numbers of elements in cache.
+    */
    private int maxSize;
 
+   /**
+    * The amount of time (in milliseconds) an element is not written or
+    * read before it is evicted.
+    */
    private long liveTime;
 
+   /**
+    * Indicates if the cache is distributed
+    */
    private boolean distributed;
 
+   /**
+    * Indicates if the cache is replicated
+    */
    private boolean replicated;
 
+   /**
+    * The full qualified name of the cache implementation to use
+    */
    private String implementation;
 
-   private String type;
-
+   /**
+    * Indicates if the log is enabled
+    */
    private boolean logEnabled;
 
    public String getName()
@@ -93,8 +118,6 @@ public class ExoCacheConfig implements Cloneable
       distributed = b;
    }
 
-   // public void setDistributed(String b) { distributed_ = "true".equals(b) ; }
-
    public boolean isRepicated()
    {
       return replicated;
@@ -113,16 +136,6 @@ public class ExoCacheConfig implements Cloneable
    public void setImplementation(String alg)
    {
       implementation = alg;
-   }
-
-   public void setType(String type)
-   {
-      this.type = type;
-   }
-
-   public String getType()
-   {
-      return type;
    }
 
    public boolean isLogEnabled()
