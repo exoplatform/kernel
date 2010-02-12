0ca2899a1369f0af1b32e6bff13e499d84a20ef5
Julien Viet <julien.viet@exoplatform.com>
2010-02-12 15:17:37 +0000

    EXOJCR-506 : Define the RESTEndpoint annotation for exposing a managed service as a REST endpoint

---
+++ exo.kernel.container/src/main/java/org/exoplatform/management/management/annotations/RESTEndpoint.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.exoplatform.management.management.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotates a managed service to define its exposure as a rest endpoint. The managed service must already be annotated
+ * with {@link org.exoplatform.management.annotations.Managed}.
+ *
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface RESTEndpoint
+{
+
+   /**
+    * Returns the rest path of the managed object.
+    *
+    * @return the path
+    */
+   String path();
+
+}
