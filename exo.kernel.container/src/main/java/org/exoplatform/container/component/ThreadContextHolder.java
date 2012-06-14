3c738303373e9b73f4680eda894a7aa8316b7b67
Nicolas Filotto <nfilotto@exoplatform.com>
2012-06-14 16:42:02 +0000

    KER-193: The full thread context should be transfered in case of asynchronous listener

---
+++ exo.kernel.container/src/main/java/org/exoplatform/container/component/ThreadContextHolder.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2012 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.container.component;
+
+import java.security.Permission;
+
+/**
+ * <p>Defines a component that holds variables of type {@link ThreadLocal}
+ * whose value is needed by the component to work normally. 
+ * This component is mainly used when we want to do a task asynchronously.
+ * To ensure that the task will be executed in the same conditions as if it would be 
+ * executed synchronously we need to transfer the thread context from the original
+ * thread to the executor thread.</p>
+ * 
+ * @author <a href="mailto:nfilotto@exoplatform.com">Nicolas Filotto</a>
+ * @version $Id$
+ *
+ */
+public interface ThreadContextHolder
+{
+   public static final Permission MANAGE_THREAD_LOCAL = new RuntimePermission("manageThreadLocal"); 
+
+   /**
+    * Gives the value corresponding to the context of the thread
+    * @return
+    */
+   ThreadContext getThreadContext();
+}
