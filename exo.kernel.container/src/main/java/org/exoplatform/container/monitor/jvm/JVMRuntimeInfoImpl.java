c5fe2f8b5eb8c9ef9304fe6102faf5956767c22c
aymen <aymen.boughzela@exoplatform.com>
2013-03-27 12:05:45 +0100

    KER-235 : Default maxClauseCount (1024) is exceeded with lucene BooleanQuery

--- exo.kernel.container/src/main/java/org/exoplatform/container/monitor/jvm/JVMRuntimeInfoImpl.java
+++ exo.kernel.container/src/main/java/org/exoplatform/container/monitor/jvm/JVMRuntimeInfoImpl.java
@@ -45,19 +45,19 @@ public class JVMRuntimeInfoImpl implements JVMRuntimeInfo, Startable
    public JVMRuntimeInfoImpl(InitParams params) throws Exception
    {
       mxbean_ = ManagementFactory.getRuntimeMXBean();
-      PropertiesParam param = null;
       if (params != null)
       {
-         param = params.getPropertiesParam("add.system.properties");
-      }
-      if (param != null)
-      {
-         ExoProperties props = param.getProperties();
-         Iterator i = props.entrySet().iterator();
-         while (i.hasNext())
+         PropertiesParam param = params.getPropertiesParam("add.system.properties");
+
+         if (param != null)
          {
-            Map.Entry entry = (Map.Entry)i.next();
-            PrivilegedSystemHelper.setProperty((String)entry.getKey(), (String)entry.getValue());
+            ExoProperties props = param.getProperties();
+            Iterator i = props.entrySet().iterator();
+            while (i.hasNext())
+            {
+               Map.Entry entry = (Map.Entry)i.next();
+               PrivilegedSystemHelper.setProperty((String)entry.getKey(), (String)entry.getValue());
+            }
          }
       }
 
