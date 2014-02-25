c1be916533b9ef658e0d5f6003ae215262111808
aymen <aboughzela@exoplatform.com>
2014-02-25 11:38:53 +0100

    JCR-2265 : Evaluate HikariCP

--- exo.kernel.component.common/src/main/java/org/exoplatform/services/hikari/HikariDataSourceFactory.java
+++ exo.kernel.component.common/src/main/java/org/exoplatform/services/hikari/HikariDataSourceFactory.java
@@ -37,27 +37,6 @@ import javax.sql.DataSource;
  */
 public class HikariDataSourceFactory implements ObjectFactory
 {
-   private final static String PROP_ACQUIREINCREMENT = "acquireIncrement";
-   private final static String PROP_ACQUIRERETRIES = "acquireRetries";
-   private final static String PROP_ACQUIRERETRYDELAY = "acquireRetryDelay";
-   private final static String PROP_CONNECTIONTIMEOUT = "connectionTimeout";
-   private final static String PROP_IDLETIMEOUT = "idleTimeout";
-   private final static String PROP_LEAKDETECTIONTHRESHOLD = "leakDetectionThreshold";
-   private final static String PROP_MAXLIFETIME = "maxLifetime";
-   private final static String PROP_MAXPOOLSIZE = "maxPoolSize";
-   private final static String PROP_MINPOOLSIZE = "minPoolSize";
-   private final static String PROP_TRANSACTIONISOLATION = "transactionIsolation";
-   private final static String PROP_CONNECTIONCUSTOMIZERCLASSNAME = "connectionCustomizerClassName";
-   private final static String PROP_CONNECTIONINITSQL = "connectionInitSql";
-   private final static String PROP_CONNECTIONTESTQUERY = "connectionTestQuery";
-   private final static String PROP_POOLNAME = "poolName";
-   private final static String PROP_ISAUTOCOMMIT = "isAutoCommit";
-   private final static String PROP_DATASOURCECLASSNAME = "dataSourceClassName";
-   private final static String PROP_ISINITIALIZATIONFAILFAST = "isInitializationFailFast";
-   private final static String PROP_ISJDBC4CONNECTIONTEST = "isJdbc4connectionTest";
-   private final static String PROP_ISREGISTERMBEANS = "isRegisterMbeans";
-
-
    /**
     * @inheritDoc
     */
@@ -74,7 +53,6 @@ public class HikariDataSourceFactory implements ObjectFactory
          return null;
       }
 
-      Properties dataSourceProperties = new Properties();
       Properties properties = new Properties();
       for (int i = 0; i < ref.size(); i++)
       {
@@ -83,146 +61,15 @@ public class HikariDataSourceFactory implements ObjectFactory
          if (ra != null)
          {
             String propertyValue = ra.getContent().toString();
-            if (propertyName.startsWith("dataSource."))
-            {
-               dataSourceProperties.setProperty(propertyName.substring("dataSource.".length()), propertyValue);
-            }
-            else
-            {
-               properties.setProperty(propertyName, propertyValue);
-            }
+            properties.setProperty(propertyName, propertyValue);
          }
       }
-      return createDataSource(properties, dataSourceProperties);
+      return createDataSource(properties);
    }
 
-
-   public static DataSource createDataSource(Properties properties, Properties dataSourceProperties) throws Exception
+   public static DataSource createDataSource(Properties properties) throws Exception
    {
-      HikariConfig config = new HikariConfig();
-
-      String value;
-
-      value = properties.getProperty(PROP_DATASOURCECLASSNAME);
-      if (value != null)
-      {
-         config.setDataSourceClassName(value);
-      }
-
-      for (Object propKey : dataSourceProperties.keySet())
-      {
-         String propName = propKey.toString();
-         Object propValue = dataSourceProperties.get(propKey);
-         config.addDataSourceProperty(propName, propValue);
-      }
-
-      value = properties.getProperty(PROP_ACQUIREINCREMENT);
-      if (value != null)
-      {
-         config.setAcquireIncrement(Integer.parseInt(value));
-      }
-
-      value = properties.getProperty(PROP_ACQUIRERETRIES);
-      if (value != null)
-      {
-         config.setAcquireRetries(Integer.parseInt(value));
-      }
-
-      value = properties.getProperty(PROP_ACQUIRERETRYDELAY);
-      if (value != null)
-      {
-         config.setAcquireRetryDelay(Integer.parseInt(value));
-      }
-
-      value = properties.getProperty(PROP_CONNECTIONTIMEOUT);
-      if (value != null)
-      {
-         config.setConnectionTimeout(Integer.parseInt(value));
-      }
-
-      value = properties.getProperty(PROP_IDLETIMEOUT);
-      if (value != null)
-      {
-         config.setIdleTimeout(Integer.parseInt(value));
-      }
-
-      value = properties.getProperty(PROP_LEAKDETECTIONTHRESHOLD);
-      if (value != null)
-      {
-         config.setLeakDetectionThreshold(Integer.parseInt(value));
-      }
-
-      value = properties.getProperty(PROP_MAXLIFETIME);
-      if (value != null)
-      {
-         config.setMaxLifetime(Integer.parseInt(value));
-      }
-
-      value = properties.getProperty(PROP_MAXPOOLSIZE);
-      if (value != null)
-      {
-         config.setMaximumPoolSize(Integer.parseInt(value));
-      }
-
-      value = properties.getProperty(PROP_MINPOOLSIZE);
-      if (value != null)
-      {
-         config.setMinimumPoolSize(Integer.parseInt(value));
-      }
-
-      value = properties.getProperty(PROP_TRANSACTIONISOLATION);
-      if (value != null)
-      {
-         config.setTransactionIsolation(value);
-      }
-
-      value = properties.getProperty(PROP_CONNECTIONCUSTOMIZERCLASSNAME);
-      if (value != null)
-      {
-         config.setConnectionCustomizerClassName(value);
-      }
-
-      value = properties.getProperty(PROP_CONNECTIONINITSQL);
-      if (value != null)
-      {
-         config.setConnectionInitSql(value);
-      }
-
-      value = properties.getProperty(PROP_CONNECTIONTESTQUERY);
-      if (value != null)
-      {
-         config.setConnectionTestQuery(value);
-      }
-
-      value = properties.getProperty(PROP_POOLNAME);
-      if (value != null)
-      {
-         config.setPoolName(value);
-      }
-
-      value = properties.getProperty(PROP_ISAUTOCOMMIT);
-      if (value != null)
-      {
-         config.setAutoCommit(Boolean.getBoolean(value));
-      }
-
-      value = properties.getProperty(PROP_ISINITIALIZATIONFAILFAST);
-      if (value != null)
-      {
-         config.setInitializationFailFast(Boolean.getBoolean(value));
-      }
-
-      value = properties.getProperty(PROP_ISJDBC4CONNECTIONTEST);
-      if (value != null)
-      {
-         config.setJdbc4ConnectionTest(Boolean.getBoolean(value));
-      }
-
-      value = properties.getProperty(PROP_ISREGISTERMBEANS);
-      if (value != null)
-      {
-         config.setRegisterMbeans(Boolean.getBoolean(value));
-      }
+      HikariConfig config = new HikariConfig(properties);
 
       return new HikariDataSource(config);
    }
