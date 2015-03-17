97abfe5b81185ea14da4990b1bbc03c597f705e6
aboughzela <aboughzela@exoplatform.com>
2015-03-17 23:52:22 +0100

    KER-305 : Fix the violations found by Sonar

--- exo.kernel.container/src/main/java/org/exoplatform/test/MockServletContext.java
+++ exo.kernel.container/src/main/java/org/exoplatform/test/MockServletContext.java
@@ -18,6 +18,8 @@
  */
 package org.exoplatform.test;
 
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -40,6 +42,9 @@ import javax.servlet.ServletException;
  */
 public class MockServletContext implements ServletContext
 {
+
+   private static final Log LOG = ExoLogger.getLogger("org.exoplatform.test.MockServletContext");
+
 
    private String name_;
 
@@ -131,7 +136,7 @@ public class MockServletContext implements ServletContext
       }
       catch (IOException e)
       {
-         e.printStackTrace();
+         LOG.error(e);
       }
       return null;
    }
